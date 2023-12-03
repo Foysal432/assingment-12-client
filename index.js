cfb3dc232e55350f65ce7e97723942c9388a7e1e
foysal432 <foysalarham69@gmail.com>
2023-12-03 17:14:55 +0600

    add new mwthood

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const app =express();
 const cors =require('cors')
+const jwt =require('jsonwebtoken');
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ app.use(express.json());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdlhstb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,9 +28,17 @@ async function run() {
     // create collection
 const adderequest =client.db('edonDb').collection('trequest');
 const userCollection =client.db('edonDb').collection('users');
+const addedClassCollection =client.db('edonDb').collection('addedClass');
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    // JWT RELATED API
+    app.post('/jwt', async(req,res)=>{
+      const user =req.body;
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h'})
+        res.send({token});
+    })
 // user related api
 app.post('/users',async (req,res)=>{
   const user =req.body;
@@ -40,9 +49,39 @@ app.post('/users',async (req,res)=>{
   }
   const result =await userCollection.insertOne(user)
   res.send(result)
+  console.log(result);
+})
+app.get('/users',async(req,res)=>{
+  const coursol = userCollection.find();
+const result = await coursol.toArray();
+  res.send(result)
+console.log(result);
+})
+// make admin
+app.patch('/users/admin/:id',async (req,res)=>{
+  const id =req.params.id;
+  const filter ={_id: new ObjectId(id)};
+  const updatedDoc = {
+    $set:{
+      selectedRole:'admin'
+    }
+  }
+  const result =await userCollection.updateOne(filter,updatedDoc)
+  res.send(result);
+  console.log(result);
+})
+app.get('/users/admin/:email',async(req,res)=>{
+  const email =req.params.email;
+  const query = {email : email}
+  const user = await userCollection.findOne(query)
+ let Role =''
+  if (user) {
+    Role = user?.selectedRole
+  }
+  res.send(Role)
 })
 
-// post alladdedfoods
+// post teacherrequest
 app.get('/trequest',async(req,res)=>{
   const result =await adderequest.find().toArray();
   res.send(result)
@@ -54,10 +93,26 @@ app.post('/trequest', async(req,res)=>{
   res.send(result);
 })
 
-
-
-
-
+// added class
+app.post('/addedclass', async(req,res)=>{
+  const addedclass =req.body;
+  console.log(addedclass);
+  const result = await addedClassCollection.insertOne(addedclass)
+  res.send(result);
+})
+// get userbased addClass
+app.get('/addedclass/:email',async(req,res)=>{
+  const email = req.params.email;
+  const query ={email:email}
+  const user = addedClassCollection.find(query);
+  const result = await user.toArray() 
+  res.send(result)
+})
+// get all added data
+app.get('/addedclass',async(req,res)=>{
+  const result =await addedClassCollection.find().toArray();
+  res.send(result)
+})
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
